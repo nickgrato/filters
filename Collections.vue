17be5cee3f2c1291942a7708cc3b0a53672bf09c
Nick Grato <ngrato@gmail.com>
2022-01-11 17:17:27 -0800

    Create Collections.vue
    adding a use case to reference

---
+++ Collections.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+import { AndSpecification, OrSpecification, PriceSpecification, MegaFilter } from 'scripts/....'
+
+export default {
+  name: 'Collection',
+  methods: {
+
+    filterProducts() {
+      // Init a New MegaFilter
+      const megaFilter = new MegaFilter()
+
+      // Price
+      const price_spec = new PriceSpecification(this.filterPrice.data.applied)
+
+      // Group Specs
+      const allOptions = new OrSpecification(price_spec)
+      const finalSpec = new AndSpecification(allOptions)
+
+      this.filteredProductsPreSort = megaFilter.filter(this.products, finalSpec)
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
